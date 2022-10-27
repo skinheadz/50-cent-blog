c9362f46f370ef57fd30f8cf956bce6c8ba4ead8
skinheadz <113230343+skinheadz@users.noreply.github.com>
2022-10-26 20:27:24 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,40 +15,6 @@ export async function getServerSideProps() {
   };
 }
 
-// function handleSubmit(e) {
-//   //e.preventDefault();
-//   console.log('You clicked submit.');
-// }
-// const handleSubmit = async (e) => {
-//   e.preventDefault()
-//   const body = { author, header, message }
-//   try {
-//     const response = await fetch('/api/inquiry', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify(body)
-//     })
-//     if (response.status !== 200) {
-//       console.log('something went wrong')
-//       //set an error banner here
-//     } else {
-//       resetForm()
-//       console.log('form submitted successfully !!!')
-//       //set a success banner here
-//     }
-//     //check response, if success is false, dont take them to success page
-//   } catch (error) {
-//     console.log('there was an error submitting', error)
-//   }
-// }
-
-// const resetForm = () => {
-//   setAuthor('')
-//   setHeader('')
-//   setMessage('')
-// }
-
-
 
 export default function Home({allPostsData}) {
 
